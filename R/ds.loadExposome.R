34dec1a80c04142c7e7187d715a02f702a16cf51
ESCRI11 <escri11@yahoo.es>
2020-09-02 19:03:32 +0200

    updated doc + vignette

--- R/ds.loadExposome.R
+++ R/ds.loadExposome.R
@@ -6,9 +6,11 @@
 #' @param exposures \code{character} Name of the exposures variable on the study server
 #' @param description \code{character} Name of the description variable on the study server
 #' @param phenotypes \code{character} Name of the phenotypes variable on the study server
-#' @param description.famCol (default \code{"family"}) Index where the family's
-#' name (per exposures) if found in file "description". It can be both numeric
-#' or character.
+#' @param exposures.idcol 
+#' @param phenotypes.idcol 
+#' @param description.expCol 
+#' @param description.famCol \code{character} (default \code{"family"}) Name of column where the family's
+#' name (per exposures) if found in file "description". 
 #' @param exposures.asFactor \code{numeric} (default \code{5}) The exposures with more
 #' than this number of unique items will be considered as "continuous" while
 #' the exposures with less or equal number of items will be considered as
@@ -17,6 +19,7 @@
 #' information/warnings from the process of loading the exposome.
 #' @param object_name \code{character} (default \code{"exposome_set"}) Name assigned to the Exposome Set on the study server
 #' @param datasources a list of \code{\link{DSConnection-class}} (default \code{NULL}) objects obtained after login
+
 #'
 #' @return This function does not have an output. It creates an Exposome Set object on the study server.
 #'
@@ -24,7 +27,9 @@
 #' \dontrun{Refer to the package Vignette for examples.}
 #' 
 
-ds.loadExposome <- function(exposures, description, phenotypes, description.famCol = "family", 
+ds.loadExposome <- function(exposures, description, phenotypes, 
+                            exposures.idcol = "idcol", phenotypes.idcol = "idcol",
+                            description.expCol = "exposure", description.famCol = "family", 
                             exposures.asFactor = 5, warnings = FALSE, object_name = "exposome_set", 
                             datasources = NULL) {
   
@@ -44,15 +49,11 @@ ds.loadExposome <- function(exposures, description, phenotypes, description.famC
     datasources <- DSI::datashield.connections_find()
   }
   
-  if(class(description.famCol) == "character") {
-    cally <- paste0("loadExposomeDS(", exposures, ", ", description, ", ", phenotypes, ", '", 
-                    description.famCol, "', ", exposures.asFactor, ", ", warnings, ")")
-  }
-  else{
-    cally <- paste0("loadExposomeDS(", exposures, ", ", description, ", ", phenotypes, ", ", 
-                    description.famCol, ", ", exposures.asFactor, ", ", warnings, ")")
-  }
-  
+
+  cally <- paste0("loadExposomeDS(", exposures, ", ", description, ", ", phenotypes, ", '",
+                  exposures.idcol, "', '", phenotypes.idcol, "', '", description.expCol, "', '",
+                  description.famCol, "', ", exposures.asFactor, ", ", warnings, ")")
+
   DSI::datashield.assign.expr(datasources, object_name, as.symbol(cally))
 
 }
