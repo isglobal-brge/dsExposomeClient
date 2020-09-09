7d685b613da2d901c621fe690559840eca0bb80d
ESCRI11 <escri11@yahoo.es>
2020-09-09 12:23:22 +0200

    ds.plotFamily, 'all' plot generated inside the client

--- R/ds.plotFamily.R
+++ R/ds.plotFamily.R
@@ -4,8 +4,49 @@ ds.plotFamily <- function(x, family, group = NULL, group2 = NULL, na.omit=TRUE, 
     datasources <- DSI::datashield.connections_find()
   }
   
-  cally <- paste0("plotFamilyDS(", x, ", '", family, "', ", if(is.null(group)){NA}else{paste0("'",group,"'")}, ", ", 
-                  if(is.null(group2)){NA}else{paste0("'",group2,"'")}, ", FALSE, ", na.omit, ")")
-  pt <- DSI::datashield.aggregate(datasources, as.symbol(cally))
-  return(pt)
+  if(family == "all"){
+    
+    vplayout <- function(x, y) {
+      grid::viewport(layout.pos.row = x, layout.pos.col = y)
+    }
+    
+    nc <- round(sqrt(length(ds.familyNames(x, FALSE, datasources)[[1]])))
+    if(nc * nc < length(ds.familyNames(x, FALSE, datasources)[[1]])) {
+      nr <- nc + 1
+    } else {
+      nr <- nc
+    }
+    de
+    ff <- ds.familyNames(x, FALSE, datasources)[[1]]
+    
+    grid::grid.newpage()
+    grid::pushViewport(grid::viewport(layout = grid::grid.layout(nr, nc)))
+    
+    idx <- 1
+    for(ii in 1:nr) {
+      for(jj in 1:nc) {
+        if(idx < length(ff) + 1) {
+          cally <- paste0("plotFamilyDS(", x, ", '", ff[idx], "')")
+          plt <- DSI::datashield.aggregate(datasources, as.symbol(cally))[[1]]
+          plt <- plt + ggplot2::ggtitle(ff[idx])
+          if(jj != 1) {
+            plt <- plt + ggplot2::ylab("")
+          }
+          if(ii != nr) {
+            plt <- plt + ggplot2::xlab("")
+          }
+          print(plt, vp = vplayout(ii, jj))
+          idx <- idx + 1
+        }
+      }
+    }
+    
+  }
+  else{
+    cally <- paste0("plotFamilyDS(", x, ", '", family, "', ", if(is.null(group)){NA}else{paste0("'",group,"'")}, ", ",
+                    if(is.null(group2)){NA}else{paste0("'",group2,"'")}, ", FALSE, ", na.omit, ")")
+    pt <- DSI::datashield.aggregate(datasources, as.symbol(cally))
+
+    return(pt)
+  }
 }
