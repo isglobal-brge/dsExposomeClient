75fc01d6a18a59c49f0fbf334b61684fb0136a69
ESCRI11 <escri11@yahoo.es>
2020-09-22 18:11:58 +0200

    updated exposome pca

--- R/ds.exposome_pca.R
+++ R/ds.exposome_pca.R
@@ -7,6 +7,7 @@
 #' @param fam \code{character vector} (default \code{NULL}) Families to subset the exposome set
 #' @param standar \code{bool} Whether the values will be normalized prior the analysis (\code{TRUE}) or not (\code{FALSE})
 #' Default \code{TRUE}
+#' @param method
 #' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login
 #'
 #' @return A \code{data.frame} with principal components and variables
@@ -15,7 +16,7 @@
 #' \dontrun{Refer to the package Vignette for examples.}
 #' @export
 
-ds.exposome_pca <- function(Set, fam = NULL, standar = TRUE, datasources = NULL){
+ds.exposome_pca <- function(Set, fam = NULL, standar = TRUE, method = "normal", datasources = NULL){
   
   if(is.null(Set) | class(Set) != "character"){
     stop("Input variable 'Set' must have a value which is a character string")
@@ -30,22 +31,19 @@ ds.exposome_pca <- function(Set, fam = NULL, standar = TRUE, datasources = NULL)
     Set <- paste0(Set, "_subsetted")
   }
   
-  checkForExposomeSet(Set, datasources)
-  
-  cally <- paste0("exposures_pData(", Set, ", 'exposures')")
-  DSI::datashield.assign.expr(conns, "dta", as.symbol(cally))
-  
-  ds.subset_type("dta", "numeric", "dta", datasources)
-  
-  pca <- ds.pca("dta", standar, datasources)
-  
-  # Remove created variables on the study server
-  datashield.rm(datasources, "dta")
-  
-  if(!is.null(fam)){
-    datashield.rm(datasources, Set)
+  if(standar){
+    ds.standardize(Set, name = "pca_std_exposomeSet", method = method, datasources = datasources)
+    Set <- "pca_std_exposomeSet"
   }
   
-  return(pca)
+  checkForExposomeSet(Set, datasources)
   
+  ds.exposures_pData(Set, "all", "ds.exposome_pca.dataset", datasources)
+  
+  cally <- paste0("exposome_pcaDS(", Set, ", ", "ds.exposome_pca.dataset", ")")
+  DSI::datashield.assign.expr(datasources, "ds.exposome_pca.Results", as.symbol(cally))
+  
+  if(standar){
+    datashield.rm(datasources, Set)
+  }
 }
