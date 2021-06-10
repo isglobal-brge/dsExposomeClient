d85303636651c56f81d70e2addf00e3b7d9f4e9a
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2021-06-10 16:15:00 +0200

    HCPC + pca FAMD added

---
+++ R/ds.exposome_HCPC.R
@@ -0,0 +1,28 @@
+#' @title Hierarchical clustering on principal components
+#' @subtitle Perform a HCPC on the PCA/FAMD of an ExposomeSet
+#' 
+#' @details The suggested partition is the one with the higher relative loss of inertia (i(clusters n+1)/i(cluster n)). 
+#' Make use of the function FactoMineR::plot.HCPC() to visualize the results.
+#'
+#' @param object \code{character} Object on the server created by the function \link{ds.exposome_pca}
+#' @param nb.clust \code{numeric} (default \code{-1}) Number of clusters to find. If -1, the tree is automatically 
+#' cut at the suggested level (see details). If a (positive) integer, the tree is cut with nb.cluters clusters.
+#' @param datasources a list of \code{\link{DSConnection-class}} (default \code{NULL}) objects obtained after login
+#'
+#' @return Object created by FactoMineR::HCPC with the original dataset removed.
+#' @export
+#'
+
+ds.exposome_HCPC <- function(object, nb.clust = -1, datasources = NULL){
+  
+  if (is.null(datasources)) {
+    datasources <- DSI::datashield.connections_find()
+  }
+  
+  if(nb.clust == 0){nb.clust <- -1}
+  
+  cally <- paste0("exposome_HCPCDS(", object, ", ", nb.clust, ")")
+  ans <- DSI::datashield.aggregate(datasources, as.symbol(cally))
+  return(ans)
+
+}
--- R/ds.exposome_pca.R
+++ R/ds.exposome_pca.R
@@ -12,6 +12,9 @@
 #' and the standard variation as dispersion, \code{"robust"} which uses the median and median absolute deviation respectively
 #' and \code{"interquartile range"} which uses the median as the center and the coeficient between the 
 #' interquartile range of the exposure and the normal range between the percentile 75 and 25 as variance.
+#' @param pca \code{bool} (default \code{TRUE}) If TRUE perform PCA (only numerical variables), 
+#' if FALSE FAMD (numerical and categorical)
+#' @param \code{numeric} (default \code{10}) Number of PC to be kept
 #' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login
 #'
 #' @return This function does not have an output. It creates a object on the study server named \code{ds.exposome_pca.Results},
@@ -21,7 +24,7 @@
 #' \dontrun{Refer to the package Vignette for examples.}
 #' @export
 
-ds.exposome_pca <- function(Set, fam = NULL, standar = TRUE, method = "normal", datasources = NULL){
+ds.exposome_pca <- function(Set, fam = NULL, standar = TRUE, method = "normal", pca = TRUE, npc = 10, datasources = NULL){
   
   if(is.null(Set) | class(Set) != "character"){
     stop("Input variable 'Set' must have a value which is a character string")
@@ -34,6 +37,8 @@ ds.exposome_pca <- function(Set, fam = NULL, standar = TRUE, method = "normal", 
   if(!is.null(fam)){
     ds.exposomeSubset(Set, fam, NULL, datasources)
     Set <- paste0(Set, "_subsetted")
+    warning('The family subset of [', paste0(fam, collapse = ", "), '] yielded (', 
+            unlist(ds.dim(Set)[1])[1], ') valid exposures.')
   }
   
   if(standar){
@@ -43,7 +48,7 @@ ds.exposome_pca <- function(Set, fam = NULL, standar = TRUE, method = "normal", 
   
   checkForExposomeSet(Set, datasources)
   
-  cally <- paste0("exposome_pcaDS(", Set, ")")
+  cally <- paste0("exposome_pcaDS(", Set, ", npc = ", npc, ", pca = ", pca, ")")
   DSI::datashield.assign.expr(datasources, "ds.exposome_pca.Results", as.symbol(cally))
   
   if(standar){
