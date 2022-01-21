339d15d506a0a287cd7186640a12dbaae0314411
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2022-01-21 16:18:39 +0100

    fixed bug with only one covariate to study

--- R/ds.exwas.R
+++ R/ds.exwas.R
@@ -33,31 +33,46 @@ ds.exwas <- function(model, Set, family, tef = TRUE, datasources = NULL) {
   cally <- paste0("exposureNamesDS(", Set, ")")
   exposure_names <- DSI::datashield.aggregate(datasources, as.symbol(cally))
 
-  form <- as.character(formula(model))
-
+  # form <- as.character(formula(model))
+  form <- formula(model)
+  
   # Copy 'dta' into 'dta_all', 'dta' will be modified on each loop iteration
   DSI::datashield.assign.expr(datasources, symbol = "dta_all", quote(dta))
 
   items <- NULL
   for (exposure in exposure_names[[1]]) {# exposure_names) {
     # Build formula: pheno_objective ~ exposure + adjusting phenotypes
-    frm <- as.formula(paste0(form[2], "~", exposure, "+", form[3]))
+    frm <- as.formula(paste0(form[[2]], "~", exposure, "+", form[[3]]))
     # Get column indexes of pheno_objective, exposure and adjusting phenotypes
     cols_to_keep <- as.numeric(which(unlist(ds.colnames("dta_all")[1]) %in% all.vars(frm)))
     # Overwrite 'dta' with only the columns of pheno_objective, exposure and adjusting phenotypes
     # taken from 'dta_all'
-    cally <- paste0("subsetDS(dt = 'dta_all', cs = c(",  paste0(cols_to_keep, collapse = ", "), "), complt = FALSE)")
-    DSI::datashield.assign.expr(datasources, "dta", as.symbol(cally))
-
-    # To do: Filter out rows that contain NAs from 'dta'!
+      # Create rep of the length of the table to be used on the dataFrameSubset to keep all the rows
+      ds.rep(x1 = 0, length.out = ds.dim("dta_all", datasources = datasources)[[1]][1],
+             source.x1 = "clientside", source.times = "c", source.length.out = "c",
+             source.each = "c", newobj = "ZEROES", datasources = datasources)
+      ds.dataFrameSubset(df.name = "dta_all",
+                         V1.name = "ZEROES",
+                         V2.name = "ZEROES",
+                         Boolean.operator = "==",
+                         keep.cols = cols_to_keep,
+                         rm.cols = NULL,
+                         keep.NAs = FALSE,
+                         newobj = "dta",
+                         datasources = datasources,
+                         notify.of.progress = FALSE)
+      
+      # TODO Filter out rows that contain NAs from 'dta'!????!
 
     tryCatch({
       # Fit GLM using the non-disclosive function
+      # TODO meta/pooled
       mod <- ds.glm(frm, family = family, data = 'dta', viewIter = FALSE)
+      # mod <- ds.glmerSLMA(frm, family = family, data = 'dta', viewIter = FALSE)
       items <- rbind(items, cbind(exposure, mod$coefficients[2, 1], mod$coefficients[2, 5], 
                                   mod$coefficients[2, 6], mod$coefficients[2, 4]))
       # return(mod$coefficients)
-    }, error = function(e){print("ds.GLM error")})
+    }, error = function(e){print(datashield.errors())})
 
   }
 
@@ -95,3 +110,4 @@ ds.exwas <- function(model, Set, family, tef = TRUE, datasources = NULL) {
   return(list(exwas_results = items, alpha_corrected = alpha_corrected))
 
 }
+
