780101b0f22450f2f6cea5cdf17170e775c4016d
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2022-02-18 15:30:20 +0100

    [ci skip] .

--- .azure/prepare.bash
+++ .azure/prepare.bash
@@ -5,7 +5,7 @@ echo "Create user libraries directory R [ ${R_LIBS_USER} ]"
 mkdir -p "${R_LIBS_USER}"
 echo "Install required Linux dependencies"
 sudo apt-get update
-sudo apt-get install libudunits2-dev
+sudo apt-get -qq -y install libudunits2-dev
 echo "Install required R dependencies"
 Rscript -e "install.packages(c('git2r', 'covr', 'withr', 'devtools', 'lintr', 'mockery', 'pkgdown', 'corrplot', 'circlize', 'FactoMineR', 'units', 'sf'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
 Rscript -e "install.packages(c('DSI', 'DSOpal'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
