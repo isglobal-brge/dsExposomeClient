f304d3cc55a4b9d4172255ffb415f2c8bd9efa40
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2022-02-18 13:09:38 +0100

    [ci skip] .

--- .azure/prepare.bash
+++ .azure/prepare.bash
@@ -4,7 +4,7 @@ AGENT_USER_HOMEDIRECTORY=$(echo "${AGENT_HOMEDIRECTORY}" | cut -d/ -f 1-3)
 echo "Create user libraries directory R [ ${R_LIBS_USER} ]"
 mkdir -p "${R_LIBS_USER}"
 
-Rscript -e "install.packages(c('git2r', 'covr', 'withr', 'devtools', 'lintr', 'mockery', 'pkgdown', 'corrplot', 'circlize', 'FactoMineR'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
+Rscript -e "install.packages(c('git2r', 'covr', 'withr', 'devtools', 'lintr', 'mockery', 'pkgdown', 'corrplot', 'circlize', 'FactoMineR', 'sf'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
 Rscript -e "install.packages(c('DSI', 'DSOpal'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
 Rscript -e "install.packages(c('BiocManager'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
 Rscript -e "BiocManager::install('BiocStyle', lib='${R_LIBS_USER}')"
