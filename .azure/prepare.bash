b2550a93f32f2b53d63076f32740b77642d0ae64
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2022-02-18 14:01:06 +0100

    [ci skip] .

--- .azure/prepare.bash
+++ .azure/prepare.bash
@@ -3,9 +3,9 @@
 AGENT_USER_HOMEDIRECTORY=$(echo "${AGENT_HOMEDIRECTORY}" | cut -d/ -f 1-3)
 echo "Create user libraries directory R [ ${R_LIBS_USER} ]"
 mkdir -p "${R_LIBS_USER}"
-
+echo "Install required Linux dependencies"
 apt-get install libudunits2-dev
-
+echo "Install required R dependencies"
 Rscript -e "install.packages(c('git2r', 'covr', 'withr', 'devtools', 'lintr', 'mockery', 'pkgdown', 'corrplot', 'circlize', 'FactoMineR', 'units', 'sf'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
 Rscript -e "install.packages(c('DSI', 'DSOpal'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
 Rscript -e "install.packages(c('BiocManager'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
